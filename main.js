8fa3ebc650928fd967252f0967f16c5e3567f84f
IronManKoa <ironmankoa14@gmail.com>
2021-09-23 01:29:35 +0530

    First layout of homepage

---
+++ main.js
@@ -0,0 +1,8 @@
+function generateDiv(color)
+{
+  
+ div= document.createElement(div)
+ div.style.color=color
+}
+colors=['red','green','blue','purple','black','brown','yellow','orange','lightblue'];
+forEach(colors,generateDiv)
